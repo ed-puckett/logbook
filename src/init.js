9f1ce86023863fa4a71aaae27ca9d5c8fe0b5929
Ed Puckett <ed.puckett@gmail.com>
2023-08-02 03:28:10 -0600

    WIP

--- src/init.js
+++ src/init.js
@@ -1,5 +1,5 @@
 // ensure that the various custom elements have been defined
-// this is not entirely necessary but gets stylesheets, etc loaded
+// this is not entirely necessary but gets stylesheets, etc loaded right away
 import './toggle-switch-element/_.js';
 import './tool-bar-element/_.js';
 import './editor-cell-element/_.js';
@@ -14,7 +14,9 @@ if (document.readyState === 'interactive') {
 } else {
     window.addEventListener('load', (load_event) => {
         trigger_document_initialization();
-    }, { once: true });
+    }, {
+        once: true,
+    });
 }
 
 function trigger_document_initialization() {
