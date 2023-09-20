60788355c72e4b48b4548ef0e549a19870b677ef
Ed Puckett <ed.puckett@gmail.com>
2023-09-20 13:55:12 -0600

    WIP

--- src/renderer/plotly-renderer.js
+++ src/renderer/plotly-renderer.js
@@ -10,11 +10,29 @@ import {
 export class PlotlyRenderer extends Renderer {
     static type = 'plotly';
 
-    // Format of config object: { data, layout, config, frames }
+    // Format of config object: { data, layout?, config?, frames? } or simply data
     // (the sub-objects layout, config and frames are optional)
 
     // may throw an error
-    async render(output_context, config, options) {
+    async render(output_context, config_, options) {
+        if (typeof config_ !== 'object') {
+            throw new Error('config_ must be an object');
+        }
+
+        let data, layout, config, frames;
+        if ('data' in config_ && Array.isArray(config_.data)) {
+            ({
+                data,
+                layout = {},
+                config = {},
+                frames = [],
+            } = config_);
+        } else {
+            data = config_;
+            config = {};
+        }
+        config.displayModeBar = false;  // remove icons/links from top-right of plot
+
         const style = options?.style;
 
         const parent = output_context.create_child({
@@ -25,7 +43,7 @@ export class PlotlyRenderer extends Renderer {
         const output_element = output_context.constructor.create_element_child(parent, {
             style,
         });
-        await Plotly.newPlot(output_element, config);  // render to the output_element
+        await Plotly.newPlot(output_element, { data, layout, config, frames });  // render to the output_element
 
         return parent;
     }
