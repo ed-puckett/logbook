145102225e7abf01d493a35a71d6e798206963a6
Ed Puckett <ed.puckett@gmail.com>
2023-09-27 12:49:28 -0600

    WIP

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -16,14 +16,18 @@ export class Renderer extends StoppableObjectsManager {
 
     get type (){ return this.constructor.type }
 
+    /** to be implemented by subclasses
+     */
     async render(output_context, value, options) {
-        // to be implemented by subclasses
         throw new Error('NOT IMPLEMENTED');
     }
 
-    static class_from_type(type)    { return renderer_class_from_type(type); }
-    static get_classes()            { return get_renderer_classes(); }
-    static set_classes(new_classes) { set_renderer_classes(new_classes); }
-    static add_class(rc)            { add_renderer_class(rc); }
-    static remove_class(rc)         { remove_renderer_class(rc); }
+    static class_from_type(type)       { return renderer_class_from_type(type); }
+    static get_classes()               { return get_renderer_classes(); }
+    static set_classes(new_classes)    { set_renderer_classes(new_classes); }
+    static add_class(rc)               { add_renderer_class(rc); }
+    static remove_class(rc)            { remove_renderer_class(rc); }
+
+    static get_class_types()           { return get_renderer_classes().map(rc => rc.type); }
+    static remove_class_for_type(type) { return this.remove_class(this.class_from_type(type)); }
 }
