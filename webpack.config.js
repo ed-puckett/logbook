612d12d73bdc5ea3d8597909a3a67215ba3f04e7
ed <ed.puckett@gmail.com>
2024-06-05 15:15:28 -0600

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const current_script_url = import.meta.url;  // save for later
 
 import { fileURLToPath } from "node:url";
 
-import { CycloneDxWebpackPlugin } from '@cyclonedx/webpack-plugin';
-
 const dist_dir_path = fileURLToPath(new URL("./dist", current_script_url));
 
 const webpack_config = {
@@ -24,14 +22,6 @@ const webpack_config = {
     },
 
     devtool: 'source-map',
-
-    plugins: [
-        new CycloneDxWebpackPlugin({
-            specVersion: '1.4',
-            outputLocation: './cyclonedx-sbom',
-            reproducibleResults: true,
-        }),
-    ],
 
     module: {
         rules: [
